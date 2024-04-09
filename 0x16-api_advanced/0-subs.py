0d46b8d8a2e40d559a57a862d43a93a725b32368
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-04-09 23:05:39 +0100

    task 0 created

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Contains a function that works with Reddit API
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'solayof'}
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 200:
+        res = res.json()
+        subs = res.get('data').get('subscribers')
+        return subs
+    return 0
+
+if __name__ == "__main__":
+    print("number of programming subscribers: {}".format(
+        number_of_subscribers("programming")))
+    print("number of fake_subreddit subscribers: {}".format(
+        number_of_subscribers("fake_subreddit")))
