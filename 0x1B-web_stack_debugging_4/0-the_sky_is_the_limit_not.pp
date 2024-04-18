a79474edd8a6ead4d578ae6074dcd53b29b4c234
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-04-18 17:08:30 +0100

    task 0 changed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,11 @@
-#change the ulimits for nginx
+# puppet code that sets nginx ULIMIT
+exec { 'Fix-nginx-ulimit':
+  command => '/bin/sed -i "s/^ULIMIT=\"-n 15\"/ULIMIT=\"-n 768\"/" /etc/default/nginx',
+  onlyif  => '/bin/grep -q \'ULIMIT="-n 15"\' /etc/default/nginx',
+  path    => '/bin/',
+  before  => Exec['restart-nginx'],
+}
 
-exec {'change limit on open files'
-  command => 'sed -i "s/15/10000/" /etc/defualt/nginx && service nginx restart',
-    onlyif  => '/bin/grep -q \'ULIMIT="-n 15"\' /etc/default/nginx',
-  path   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+exec { 'restart-nginx':
+  command => '/usr/bin/sudo /usr/sbin/service nginx restart',
 }
