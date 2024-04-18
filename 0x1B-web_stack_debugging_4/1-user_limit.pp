23e347bbaa35e57ff58670b3d93e034b5d11446b
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-04-18 16:38:10 +0100

    task 2 created

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,11 @@
 #change the ulimits for nginx
 
-exec {"change limit on open files"
-	command => 'sed -i "s/15/10000/" /etc/defualt/nginx && service nginx restart',
+exec {"change the hard nofile"
+	command => 'sed -i "s/5/1000/" /etc/security/limits.conf',
 	path 	=> "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+}
+
+exec {"change the soft nofile"
+        command => 'sed -i "s/4/1000/" /etc/security/limits.conf',
+        path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 }
