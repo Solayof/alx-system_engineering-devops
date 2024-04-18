a31d21a88689dc457ff5728cdbc6b3d13b56b5d5
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-04-18 16:42:07 +0100

    puppet lint error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 #change the ulimits for nginx
 
-exec {"change limit on open files"
-	command => 'sed -i "s/15/10000/" /etc/defualt/nginx && service nginx restart',
-	path 	=> "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+exec {'change limit on open files'
+  command => 'sed -i "s/15/10000/" /etc/defualt/nginx && service nginx restart',
+  path   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 #change the ulimits for nginx
 
-exec {"change the hard nofile"
-	command => 'sed -i "s/5/1000/" /etc/security/limits.conf',
-	path 	=> "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+exec {'change the hard nofile'
+  command => 'sed -i "s/5/1000/" /etc/security/limits.conf',
+  path   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-exec {"change the soft nofile"
+exec {'change the soft nofile'
         command => 'sed -i "s/4/1000/" /etc/security/limits.conf',
-        path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+        path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
